28dd8bbfe426739d06896a72596aa321c5f41d77
Guy Royse <guy@guyroyse.com>
2021-10-07 21:57:01 -0400

    separate runner for unit and functional tests

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,33 @@
 module.exports = {
-
-  roots: [
-    "<rootDir>/lib/",
-    "<rootDir>/spec/"
-  ],
-
-  clearMocks: true,
-  
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-      diagnostics: true
+  projects: [{
+    displayName: "unit",
+    roots: [ "<rootDir>/lib/", "<rootDir>/spec/" ],
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    globals: {
+      "ts-jest": {
+        tsconfig: "tsconfig.json",
+        diagnostics: true
+      },
     },
-  },
-
-  testMatch: [
-    "**/*.spec.ts"
-  ],
-
-  transform: {
-    "^.+\\.ts$": "ts-jest"
-  },
-
-  detectOpenHandles: true
-}
+    testMatch: [ "**/*.spec.ts" ],
+    transform: { "^.+\\.ts$": "ts-jest" },
+    detectOpenHandles: true
+  }, {
+    displayName: "functional",
+    roots: [ "<rootDir>/lib/", "<rootDir>/functional/" ],
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    globals: {
+      "ts-jest": {
+        tsconfig: "tsconfig.json",
+        diagnostics: true
+      },
+    },
+    testMatch: [ "**/*.spec.ts" ],
+    transform: { "^.+\\.ts$": "ts-jest" },
+    detectOpenHandles: true
+  }]
+};
