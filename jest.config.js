f06d61a7ea73126d349bdc746470cc84b76a4f59
Guy Royse <guy@guyroyse.com>
2021-10-21 12:06:35 -0400

    fixed coverage not running

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  projects: [{
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+projects: [{
     displayName: "unit",
     roots: [ "<rootDir>/lib/", "<rootDir>/spec/unit/" ],
     clearMocks: true,
-    collectCoverage: true,
-    coverageDirectory: "coverage/unit",
     globals: {
       "ts-jest": {
         tsconfig: "tsconfig.json",
@@ -18,8 +18,6 @@ module.exports = {
     displayName: "functional",
     roots: [ "<rootDir>/lib/", "<rootDir>/spec/functional/" ],
     clearMocks: true,
-    collectCoverage: true,
-    coverageDirectory: "coverage/functional",
     globals: {
       "ts-jest": {
         tsconfig: "tsconfig.json",
