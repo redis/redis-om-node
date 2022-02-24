39d79989ea5140efc19db1f4e394dee706e32bb6
Guy Royse <guy@guyroyse.com>
2022-02-24 14:40:50 -0500

    updated imports and exports for TypeDoc generation

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,14 @@
 import Client, { SearchDataStructure, HashData, JsonData } from "./client";
-import Entity, { EntityConstructor, EntityData } from "./entity/entity";
+import Entity, { EntityConstructor, EntityData, EntityValue } from "./entity/entity";
 import RedisError from "./errors";
 import Repository from "./repository/repository";
 import { EntityCreationData } from "./repository/repository";
 import Schema from "./schema/schema";
-import { Point, SchemaDefinition, FieldDefinition, Field, NumberField, StringField, BooleanField, PointField, StringArrayField,
-  IdStrategy, StopWordOptions } from "./schema/schema-definitions";
+import { 
+  SchemaDefinition, IdStrategy, StopWordOptions, Point, FieldDefinition, 
+  Field, BooleanField, DateField, NumberField, PointField, StringField, StringArrayField, TextField } from "./schema/schema-definitions";
 import { SchemaOptions } from "./schema/schema-options";
-import { SubSearchFunction, Search, RawSearch } from "./search/search";
+import { AbstractSearch, Search, RawSearch, SubSearchFunction } from "./search/search";
 import Where from "./search/where";
 import WhereField from "./search/where-field";
 import { Circle, CircleFunction } from "./search/where-point";
@@ -15,11 +16,11 @@ import { Circle, CircleFunction } from "./search/where-point";
 
 export {
   Client, SearchDataStructure, HashData, JsonData, 
-  Entity, EntityConstructor, EntityData,
-  RedisError,
-  Repository, EntityCreationData,
+  Entity, EntityConstructor, EntityData, EntityValue, 
+  RedisError, Repository, EntityCreationData,
   Schema, SchemaDefinition, SchemaOptions, Point,
-  FieldDefinition, Field, NumberField, StringField, BooleanField, PointField, StringArrayField, IdStrategy, StopWordOptions,
-  Search, RawSearch, SubSearchFunction,
+  FieldDefinition, IdStrategy, StopWordOptions,
+  Field, BooleanField, DateField, NumberField, PointField, StringField, StringArrayField, TextField,
+  AbstractSearch, Search, RawSearch, SubSearchFunction,
   Where, WhereField, Circle, CircleFunction
 };
