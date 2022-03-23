837584d8346fab80251384f22eb0f6fcf293bb94
Guy Royse <guy@guyroyse.com>
2022-03-23 16:26:28 -0400

    test hash and json for field getters/setters on Entity

--- spec/unit/schema/schema-fields.spec.ts
+++ spec/unit/schema/schema-fields.spec.ts
@@ -28,69 +28,75 @@ describe("Schema", () => {
   let schema: Schema<TestEntity>;
   let entity: TestEntity;
 
-  beforeAll(() => {
-    schema = new Schema<TestEntity>(TestEntity, {
-      aBoolean: { type: 'boolean' },
-      aNumber: { type: 'number' },
-      aString: { type: 'string' },
-      someText: { type: 'string' },
-      aPoint: { type: 'point' },
-      aDate: { type: 'date' },
-      someStrings: { type: 'string[]' }
-    }, { dataStructure: 'HASH' });
-  });
+  describe.each([
+    [ "when configured for HASH", 'HASH'],
+    [ "when configured for JSON", 'JSON']
+  ])("%s", (_, dataStructure) => {
 
-  beforeEach(() => {
-    let entityData: EntityData = {};
-    entityData.aBoolean = true;
-    entityData.aNumber = A_NUMBER;
-    entityData.aString = A_STRING;
-    entityData.someText = SOME_TEXT;
-    entityData.aPoint = A_POINT;
-    entityData.aDate = A_DATE;
-    entityData.someStrings = SOME_STRINGS;
-    entity = new TestEntity(schema, "some_id", entityData)
-  });
+    beforeAll(() => {
+      schema = new Schema<TestEntity>(TestEntity, {
+        aBoolean: { type: 'boolean' },
+        aNumber: { type: 'number' },
+        aString: { type: 'string' },
+        someText: { type: 'string' },
+        aPoint: { type: 'point' },
+        aDate: { type: 'date' },
+        someStrings: { type: 'string[]' }
+      }, { dataStructure: dataStructure as SearchDataStructure });
+    });
 
-  it("adds a boolean getter and setter", () => {
-    expect(entity).toHaveProperty('aBoolean', true);
-    entity.aBoolean = false;
-    expect(entity.aBoolean).toBe(false);
-  });
+    beforeEach(() => {
+      let entityData: EntityData = {};
+      entityData.aBoolean = true;
+      entityData.aNumber = A_NUMBER;
+      entityData.aString = A_STRING;
+      entityData.someText = SOME_TEXT;
+      entityData.aPoint = A_POINT;
+      entityData.aDate = A_DATE;
+      entityData.someStrings = SOME_STRINGS;
+      entity = new TestEntity(schema, "some_id", entityData)
+    });
 
-  it("adds a number getter and setter", () => {
-    expect(entity).toHaveProperty('aNumber', A_NUMBER);
-    entity.aNumber = ANOTHER_NUMBER;
-    expect(entity.aNumber).toBe(ANOTHER_NUMBER);
-  });
+    it("adds a boolean getter and setter", () => {
+      expect(entity).toHaveProperty('aBoolean', true);
+      entity.aBoolean = false;
+      expect(entity.aBoolean).toBe(false);
+    });
 
-  it("adds a string getter and setter", () => {
-    expect(entity).toHaveProperty('aString', A_STRING);
-    entity.aString = ANOTHER_STRING;
-    expect(entity.aString).toBe(ANOTHER_STRING);
-  });
+    it("adds a number getter and setter", () => {
+      expect(entity).toHaveProperty('aNumber', A_NUMBER);
+      entity.aNumber = ANOTHER_NUMBER;
+      expect(entity.aNumber).toBe(ANOTHER_NUMBER);
+    });
 
-  it("adds a text getter and setter", () => {
-    expect(entity).toHaveProperty('someText', SOME_TEXT);
-    entity.someText = SOME_OTHER_TEXT;
-    expect(entity.someText).toBe(SOME_OTHER_TEXT);
-  });
+    it("adds a string getter and setter", () => {
+      expect(entity).toHaveProperty('aString', A_STRING);
+      entity.aString = ANOTHER_STRING;
+      expect(entity.aString).toBe(ANOTHER_STRING);
+    });
 
-  it("adds a point getter and setter", () => {
-    expect(entity).toHaveProperty('aPoint', A_POINT);
-    entity.aPoint = ANOTHER_POINT;
-    expect(entity.aPoint).toEqual(ANOTHER_POINT);
-  });
+    it("adds a text getter and setter", () => {
+      expect(entity).toHaveProperty('someText', SOME_TEXT);
+      entity.someText = SOME_OTHER_TEXT;
+      expect(entity.someText).toBe(SOME_OTHER_TEXT);
+    });
 
-  it("adds a date getter and setter", () => {
-    expect(entity).toHaveProperty('aDate', A_DATE);
-    entity.aDate = ANOTHER_DATE;
-    expect(entity.aDate).toBe(ANOTHER_DATE);
-  });
+    it("adds a point getter and setter", () => {
+      expect(entity).toHaveProperty('aPoint', A_POINT);
+      entity.aPoint = ANOTHER_POINT;
+      expect(entity.aPoint).toEqual(ANOTHER_POINT);
+    });
 
-  it("adds a string[] getter and setter", () => {
-    expect(entity).toHaveProperty('someStrings', SOME_STRINGS);
-    entity.someStrings = SOME_OTHER_STRINGS;
-    expect(entity.someStrings).toEqual(SOME_OTHER_STRINGS);
+    it("adds a date getter and setter", () => {
+      expect(entity).toHaveProperty('aDate', A_DATE);
+      entity.aDate = ANOTHER_DATE;
+      expect(entity.aDate).toBe(ANOTHER_DATE);
+    });
+
+    it("adds a string[] getter and setter", () => {
+      expect(entity).toHaveProperty('someStrings', SOME_STRINGS);
+      entity.someStrings = SOME_OTHER_STRINGS;
+      expect(entity.someStrings).toEqual(SOME_OTHER_STRINGS);
+    });
   });
 });
