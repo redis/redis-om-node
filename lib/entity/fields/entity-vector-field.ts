11945ba8aaf01f1ee5fb03b55ea9cdf83c31c70c
Simon Green <simon@captaincodeman.com>
2022-06-30 21:12:00 -0600

    Vector index is only supported for HASH structures

--- lib/entity/fields/entity-vector-field.ts
+++ lib/entity/fields/entity-vector-field.ts
@@ -1,17 +1,13 @@
 import { EntityField } from "./entity-field";
-import { EntityValue } from "../entity-value";
 import { RedisHashData, RedisJsonData } from "../../client";
 
 export class EntityVectorField extends EntityField {
-  // TODO: is vector index supported with JSON? what format do we send binary data in?
   toRedisJson(): RedisJsonData {
-    const data: RedisJsonData = {};
-    if (this.value !== null) data[this.name] = data[this.name] = this.value as Buffer
-    return data;
+    throw Error(`Vector field not supported for JSON`);
   }
 
   fromRedisJson(value: any) {
-    this.value = Buffer.from(value.toString(), 'binary');
+    throw Error(`Vector field not supported for JSON`);
   }
 
   toRedisHash(): RedisHashData {
