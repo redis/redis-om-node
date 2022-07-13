cc07ab77d9d27b7cb3a50911037bb8828074f323
Guy Royse <guy@guyroyse.com>
2022-07-13 16:51:23 -0400

    adding some indexing imports

--- lib/index.ts
+++ lib/index.ts
@@ -6,13 +6,10 @@ export { EntityData } from "./entity/entity-data";
 export { EntityValue } from "./entity/entity-value";
 export { EntityConstructor } from "./entity/entity-constructor";
 
-export { Schema } from "./schema/schema";
+export { Schema } from "./schema";
 export * from "./schema/definition";
-
 export { SchemaOptions, DataStructure, IdStrategy, StopWordOptions } from "./schema/options";
-
 export { Repository } from "./repository";
-
 export { RedisError } from "./errors";
 export { AbstractSearch, Search, RawSearch, SubSearchFunction, Where, WhereField} from "./search";
 export { Circle, CircleFunction } from "./search/where-point";
---
+++ lib/schema/index.ts
@@ -0,0 +1,3 @@
+export * from './schema'
+export * from './options'
+export * from './definition'
