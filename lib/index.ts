000d2f304483d49a2efddb471c064116c46af28c
Guy Royse <guy@guyroyse.com>
2023-07-26 12:05:28 -0400

    added new field def to exports

--- lib/index.ts
+++ lib/index.ts
@@ -5,7 +5,7 @@ export { Field } from './schema/field'
 export { Schema } from './schema/schema'
 export { DataStructure, IdStrategy, SchemaOptions, StopWordOptions } from './schema/options'
 export { AllFieldDefinition, BooleanFieldDefinition, CommonFieldDefinition, DateFieldDefinition, FieldDefinition,
-  FieldType, NumberFieldDefinition, PointFieldDefinition, SchemaDefinition, StringArrayFieldDefinition,
+  FieldType, NumberArrayFieldDefinition, NumberFieldDefinition, PointFieldDefinition, SchemaDefinition, StringArrayFieldDefinition,
   StringFieldDefinition, TextFieldDefinition } from './schema/definitions'
 export { AbstractSearch, Circle, CircleFunction, RawSearch, Search, SubSearchFunction, Where, WhereField } from './search'
 export { Repository } from './repository'
